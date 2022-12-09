5bf53c2eb5411a95f8c6fd6fda1925d108e8a30b
Water23356 <710871626@qq.com>
2022-12-09 10:05:49 +0800

    图片显示bug

--- Assets/Mod_ItemPlane/Script/ItemsPlane.cs
+++ Assets/Mod_ItemPlane/Script/ItemsPlane.cs
@@ -191,6 +191,8 @@ public class ItemsPlane : MonoBehaviour
         obj.GetComponent<ItemCollider>().DescriptionPanel = ItemDescription;
         obj.GetComponent<Item>().SetData(itemsys.Items[pindex]);
         obj.GetComponent<Item>().t = pt;
+        GameObject ii = obj.transform.Find("ItemImage").gameObject;
+        ii.GetComponent<Image>().sprite = Resources.Load<Sprite>("Mod_Items/" + Items[itemindex].FileName);
         Items.Add(obj);
     }
     /// <summary>
