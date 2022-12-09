9eaa49d1dad588139438376a4382f9586fdcafa6
Water23356 <710871626@qq.com>
2022-12-09 18:18:00 +0800

    更新了静态的配方系统

--- Assets/Mod_Formula/Script/ItemFormula.cs
+++ Assets/Mod_Formula/Script/ItemFormula.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using Newtonsoft;
 using Newtonsoft.Json;
 using System.IO;
+using System.Linq;
 
 public class ItemFormula : MonoBehaviour
 {
@@ -17,8 +18,25 @@ public class ItemFormula : MonoBehaviour
     static Formula[] formulas;
 
     #region 功能
-
-    
+    /// <summary>
+    /// 获取合成物品,
+    /// </summary>
+    /// <param name="materials">添加的全部配方</param>
+    /// <param name="backM">返回的</param>
+    /// <returns></returns>
+    public static string Make(string[] materials,out string[] backM)
+    {
+        foreach(Formula formula in formulas) 
+        {
+            if(formula.isMake(materials))
+            {
+                backM = formula.getMaterials(materials);
+                return formula.Name;
+            }
+        }
+        backM = materials;
+        return "不可名状物";
+    }
 
     /// <summary>
     /// 更新配方表
@@ -28,7 +46,12 @@ public class ItemFormula : MonoBehaviour
         //读取配方表
         string formulaFile = Application.dataPath + path;
         string txt = File.ReadAllText(formulaFile);
-        formulas = JsonConvert.DeserializeObject<Formula[]>(txt);
+        FormulaT[] formulasT = JsonConvert.DeserializeObject<FormulaT[]>(txt);
+        formulas = new Formula[formulasT.Length];
+        for(int i=0;i<formulas.Length;i++)
+        {
+            formulas[i] = new Formula(formulasT[i]);
+        }
     }
     #endregion
 
@@ -53,6 +76,66 @@ public class ItemFormula : MonoBehaviour
 }
 
 class Formula
+{
+    /// <summary>
+    /// 成品名称
+    /// </summary>
+    public string Name;
+    public string[] Materials = new string[7];
+
+    public Formula(FormulaT t)
+    {
+        Name = t.Name;
+        Materials[0] = t.material1;
+        Materials[1] = t.material2;
+        Materials[2] = t.material3;
+        Materials[3] = t.material4;
+        Materials[4] = t.material5;
+        Materials[5] = t.material6;
+        Materials[6] = t.material7;
+    }
+
+    /// <summary>
+    /// 判断此材料是否可合成此物品
+    /// </summary>
+    /// <param name="materials">材料组</param>
+    /// <returns></returns>
+    public bool isMake(string[] materials)
+    {
+        foreach (string m in Materials)
+        {
+            bool mk = false;
+            if (m == "Null") { continue; }
+            foreach(string n in materials)
+            {
+                if (n == m) { mk = true; }
+            }
+            if (!mk) { return false; }
+        }
+        return true;
+    }
+    /// <summary>
+    /// 获取非自身所需材料的材料
+    /// </summary>
+    /// <returns></returns>
+    public string[] getMaterials(string[] materials)
+    {
+        List<string> list = new List<string>();
+        foreach (string m in materials)
+        {
+            bool mk = false;
+            if (m == "Null") { continue; }
+            foreach (string n in Materials)
+            {
+                if (n == m) { mk = true; }
+            }
+            if (!mk) { list.Add(m); }
+        }
+        return list.ToArray();
+    }
+}
+
+class FormulaT
 {
     /// <summary>
     /// 成品名称
@@ -86,4 +169,6 @@ class Formula
     /// 材料7名称
     /// </summary>
     public string material7;
+
+  
 }
