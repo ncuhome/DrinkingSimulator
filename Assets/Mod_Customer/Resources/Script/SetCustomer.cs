e4cf6e8187c8d4b44cfa5d0d64b165c8fc4ac2f0
sovendea77 <115879243+sovendea77@users.noreply.github.com>
2022-12-10 15:57:29 +0800

    修正了生成订单，增加点单文本显示，增加评价（未完）

--- Assets/Mod_Customer/Resources/Script/SetCustomer.cs
+++ Assets/Mod_Customer/Resources/Script/SetCustomer.cs
@@ -5,20 +5,39 @@ using UnityEngine;
 
 public class SetCustomer : MonoBehaviour
 {
+    public Material wink;
+
     private int maxc = 1;//顾客数量
 
     public ArrayList customerlist = new ArrayList();
     
     /// <summary>
-    /// 生成一个顾客
+    /// 生成一个顾客(1:人类\2：兽人\3：精灵)
     /// </summary>
     public Customer InitCutomer()
     {
         GameObject cus;
         int n = UnityEngine.Random.Range(1, 4);
         cus = Instantiate(Resources.Load("Prefb/Customer" + n.ToString())) as GameObject;
+
+        if (n == 1)
+        {
+            int m = UnityEngine.Random.Range(1, 4);
+            cus.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Image/person" + m.ToString());
+        }
+        else if (n == 2)
+        {
+            int m = UnityEngine.Random.Range(1, 3);
+            cus.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Image/animal" + m.ToString());
+        }
+        else if (n == 3)
+        {
+            //int m = UnityEngine.Random.Range(1, 3);
+            //cus.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("kk") as Sprite;
+        }
         return cus.AddComponent<Customer>();
     }
+<<<<<<< Updated upstream
     /*
     public void DelCustomer()
     {
@@ -28,24 +47,49 @@ public class SetCustomer : MonoBehaviour
         {
             //leave
         }
+=======
+
+
+    IEnumerator Wink()
+    {
+
+        //Vector4 now1 = wink.GetVector("_Param");
+        //float n = Mathf.Lerp(now1.y, 0f, 0.9f * Time.deltaTime);
+        //wink.SetVector("_Param", new Vector4(0.8f, n, 1f, 1f));
+        customerlist.Add(InitCutomer());
+        yield return new WaitForSeconds(1f);//5f
+
+
+>>>>>>> Stashed changes
     }
     */
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < maxc; i++)
-        {
-            customerlist.Add(InitCutomer());
-        }
+        wink.SetVector("_Param", new Vector4(0.8f, 1f, 1f, 1f));
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(customerlist.Count < maxc)
+        if (customerlist.Count < maxc)
         {
-            customerlist.Add(InitCutomer());
+            StartCoroutine("Wink");
+
         }
+        else
+        {
+            StopCoroutine("Wink");
+
+            Vector4 now2 = wink.GetVector("_Param");
+            float m = Mathf.Lerp(now2.y, 1f, 0.6f * Time.deltaTime);
+            wink.SetVector("_Param", new Vector4(0.8f, m, 1f, 1f));
+        }
+
+
     }
 }
