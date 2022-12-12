9d65930e7cfd3663941dd625572d21d9a6ab04d8
congxin <3038077702@qq.com>
2022-12-12 20:32:56 +0800

    光标拖动

--- Assets/Mod_Mouse/PointerChange.cs
+++ Assets/Mod_Mouse/PointerChange.cs
@@ -5,21 +5,33 @@ using UnityEngine;
 // 需要给物体加上碰撞体才能触发以下事件
 public class PointerChange : MonoBehaviour
 {
-    public Texture2D cursorTexture;
+    public Texture2D onDragTexture;
+    public Texture2D onEnterTexture;
     public CursorMode cursorMode = CursorMode.Auto;
     public Vector2 hotSpot = Vector2.zero;
 
-    // 鼠标进入物体时改变指针为 cursorTexture
-    void OnMouseEnter()
+    // 鼠标拖拽物体时改变指针为 onDragTexture
+    void OnMouseDrag()
     {
-        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
-        //Debug.Log("mouseenter");
+        Cursor.SetCursor(onDragTexture, hotSpot, cursorMode);
     }
-    // 鼠标离开物体时恢复默认指针
+
+    //鼠标移入物体时变为 onEnterTexture
+    private void OnMouseEnter()
+    {
+        Cursor.SetCursor(onEnterTexture, hotSpot, cursorMode);
+
+    }
+    //离开物体时恢复默认指针
     void OnMouseExit()
     {
         Cursor.SetCursor(null, Vector2.zero, cursorMode);
-       // Debug.Log("mouseexit");
+
+    }
+    // 拖拽物体松开时恢复默认指针
+    void OnMouseUp()
+    {
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
 
     }
 
